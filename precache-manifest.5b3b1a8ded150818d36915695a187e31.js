40c4e28bf2905c254948de853555a6a046d5fa9a
IlyasIt <ilyas.musin.172001@mail.ru>
2020-09-02 05:48:07 +0300

    Updates

---
+++ precache-manifest.5b3b1a8ded150818d36915695a187e31.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97173770312bf731245cb2183c681f23",
+    "url": "/bashwriter_33/index.html"
+  },
+  {
+    "revision": "3637b3438ab5a09c7882",
+    "url": "/bashwriter_33/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e55b5778eef83878e41d",
+    "url": "/bashwriter_33/static/css/main.5a51060a.chunk.css"
+  },
+  {
+    "revision": "3637b3438ab5a09c7882",
+    "url": "/bashwriter_33/static/js/2.2b5b65fa.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/bashwriter_33/static/js/2.2b5b65fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55b5778eef83878e41d",
+    "url": "/bashwriter_33/static/js/main.e5b3e662.chunk.js"
+  },
+  {
+    "revision": "5c57a7289f0f99c8d2dc",
+    "url": "/bashwriter_33/static/js/runtime-main.524bac97.js"
+  },
+  {
+    "revision": "966e07c9a8e1aab5388625944d48efa3",
+    "url": "/bashwriter_33/static/media/Abram.966e07c9.ttf"
+  },
+  {
+    "revision": "2b8243c15c2ff67a25082e9409869ae3",
+    "url": "/bashwriter_33/static/media/Anselmo.2b8243c1.ttf"
+  },
+  {
+    "revision": "37f48d0dffda97430540c65cab1ebadb",
+    "url": "/bashwriter_33/static/media/AvatarApp.37f48d0d.png"
+  },
+  {
+    "revision": "cfc815017f273c47bde88d64231c0a45",
+    "url": "/bashwriter_33/static/media/Benvolio.cfc81501.ttf"
+  },
+  {
+    "revision": "0f3e56753fb3a1bea256cfafff3cfa6e",
+    "url": "/bashwriter_33/static/media/Brushfont.0f3e5675.otf"
+  },
+  {
+    "revision": "96dbda11cd829e774ee82961e1494573",
+    "url": "/bashwriter_33/static/media/Capuletty.96dbda11.ttf"
+  },
+  {
+    "revision": "deb34f1be4f3b01940db47ded8818284",
+    "url": "/bashwriter_33/static/media/Djiovanni.deb34f1b.ttf"
+  },
+  {
+    "revision": "0cdf768e064af852c92590b0dd42bada",
+    "url": "/bashwriter_33/static/media/Eskal.0cdf768e.ttf"
+  },
+  {
+    "revision": "11201625065aae278e02e907c59ff3ae",
+    "url": "/bashwriter_33/static/media/Gregory.11201625.ttf"
+  },
+  {
+    "revision": "fd3fec05a4cc59a41563d13ff111ec67",
+    "url": "/bashwriter_33/static/media/Lexa.fd3fec05.ttf"
+  },
+  {
+    "revision": "9a4f01b1a86cccc522b4a322a70b44a0",
+    "url": "/bashwriter_33/static/media/Lorenco.9a4f01b1.ttf"
+  },
+  {
+    "revision": "0819c34b005f4f521ed1ab6340cd218b",
+    "url": "/bashwriter_33/static/media/Merkucio.0819c34b.ttf"
+  },
+  {
+    "revision": "3d43b4c880ba169bdc5f7951a9b2c5a4",
+    "url": "/bashwriter_33/static/media/Montekky.3d43b4c8.ttf"
+  },
+  {
+    "revision": "8e7a91accfee66949d8f243573b80839",
+    "url": "/bashwriter_33/static/media/Neucha.8e7a91ac.ttf"
+  },
+  {
+    "revision": "97fc9bd854f096b252648ce4a25bc06a",
+    "url": "/bashwriter_33/static/media/Pag.97fc9bd8.ttf"
+  },
+  {
+    "revision": "c69e67bf3c2f2dbcb0c342d43255ed23",
+    "url": "/bashwriter_33/static/media/Paris.c69e67bf.ttf"
+  },
+  {
+    "revision": "96e8bfa0661ba368237bf890592ef20a",
+    "url": "/bashwriter_33/static/media/Salavat.96e8bfa0.ttf"
+  },
+  {
+    "revision": "9ee9183ff3581db6927ec54405074a8c",
+    "url": "/bashwriter_33/static/media/Samson.9ee9183f.ttf"
+  },
+  {
+    "revision": "f2785b271a28540e4c1c7f1357f9edfc",
+    "url": "/bashwriter_33/static/media/Stefano.f2785b27.ttf"
+  },
+  {
+    "revision": "9e5b511a5881025e6d56ead8942322ee",
+    "url": "/bashwriter_33/static/media/Tibalt.9e5b511a.ttf"
+  },
+  {
+    "revision": "7aac83235ef32a95ccc7dc321d150aee",
+    "url": "/bashwriter_33/static/media/arrow.7aac8323.svg"
+  }
+]);
